cef008d9ef2585eb3561f6a486b953f81f2a6381
Alberto Campos <alberto.campos@heyirys.com>
2021-08-08 15:14:43 -0500

    added animations

--- src/App.js
+++ src/App.js
@@ -1,39 +1,11 @@
-import { useState } from "react";
-import styled, { ThemeProvider } from "styled-components";
-import Card from "./components/Card";
-import GlobalStyles from "./global.styles";
-
-const BaseTheme = {
-    color: "#222",
-    background: "#fff",
-};
-const DarkTheme = {
-    color: "#fff",
-    background: "#222",
-};
-
-const Container = styled.div`
-    padding: 2rem;
-    color: ${({ theme }) => theme.color};
-    background: ${({ theme }) => theme.background};
-`;
+import styled from "styled-components";
+import Loading from "./components/Loading";
 
 function App() {
-    const [baseTheme, setTheme] = useState(true);
-    const toggleTheme = () => {
-        setTheme((currentTheme) => !currentTheme);
-    };
     return (
-        <ThemeProvider theme={baseTheme ? BaseTheme : DarkTheme}>
-            <GlobalStyles />
-            <Container>
-                <h1>HelloWorld</h1>
-                <button onClick={toggleTheme} className="btn">
-                    toggle me
-                </button>
-            </Container>
-            {/* <Card /> */}
-        </ThemeProvider>
+        <div style={{ padding: "2rem" }}>
+            <Loading />
+        </div>
     );
 }
 
